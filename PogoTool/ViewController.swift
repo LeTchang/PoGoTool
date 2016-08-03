d541864658d4865cdad77866a6daaa1924c6033c
LeTchang <mvallet@student.42.fr>
2016-08-03 16:37:02 +0200

    Get all pokemon data

--- PogoTool/ViewController.swift
+++ PogoTool/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import PGoApi
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PGoAuthDelegate, PGoApiDelegate {
 
+    var auth: PtcOAuth!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        auth = PtcOAuth()
+        auth.delegate = self
+        auth.login(withUsername: "", withPassword: "")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func didReceiveAuth() {
+        print("Auth received!!")
+        print("Starting simulation...")
+        let request = PGoApiRequest()
+        request.simulateAppStart()
+        request.makeRequest(.Login, auth: auth, delegate: self)    }
+    
+    func didNotReceiveAuth() {
+        print("Failed to auth!")
     }
-
-
+    
+    func didReceiveApiResponse(intent: PGoApiIntent, response: PGoApiResponse) {
+        print("Got that API response: \(intent)")
+        switch intent {
+        case .Login:
+            auth.endpoint = "https://\((response.response as! Pogoprotos.Networking.Envelopes.ResponseEnvelope).apiUrl)/rpc"
+            print("New endpoint: \(auth.endpoint)")
+            let request = PGoApiRequest()
+            request.getInventory()
+            request.makeRequest(.GetInventory, auth: auth, delegate: self)
+        case .GetInventory:
+            print("Got Inventory !")
+            let r = response.subresponses[0] as! Pogoprotos.Networking.Responses.GetInventoryResponse
+            let item = r.inventoryDelta
+            for x in 0 ..< item.inventoryItems.count {
+                if item.inventoryItems[x].inventoryItemData.hasPokemonData == true {
+                    if item.inventoryItems[x].inventoryItemData.pokemonData.hasIsEgg == false {
+                        let id = item.inventoryItems[x].inventoryItemData.pokemonData.id
+                        var pkmn = item.inventoryItems[x].inventoryItemData.pokemonData.pokemonId.description
+                        pkmn.removeAtIndex(pkmn.startIndex)
+                        let num = String(format: "%03d", item.inventoryItems[x].inventoryItemData.pokemonData.pokemonId.rawValue)
+                        let atq = item.inventoryItems[x].inventoryItemData.pokemonData.individualAttack
+                        let def = item.inventoryItems[x].inventoryItemData.pokemonData.individualDefense
+                        let sta = item.inventoryItems[x].inventoryItemData.pokemonData.individualStamina
+                        let perf = String(format: "%.2f", Float(atq + def + sta) / 45.0 * 100.0) + "%"
+                        print("ID:", id, "| Num:", num)
+                        print(pkmn, " | ", atq, "/", def, "/", sta, " | perfect: ", perf, separator: "", terminator: "\n\n")
+                    }
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+    func didReceiveApiError(intent: PGoApiIntent, statusCode: Int?) {
+        print("API Error: \(statusCode)")
+    }
 }
 
